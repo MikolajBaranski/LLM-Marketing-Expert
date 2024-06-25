a4f7097d9119f675b0ee8f165ba9237f10abaacb
MikolajBaranski <mikibaranski@gmail.com>
2024-06-25 11:04:17 +0200

    Minor cleaning of code structure.

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
 # Default parameters for testing
-IMAGE_RAW_PATH = "assets/image3.png"
-IMAGE_HEATMAP_PATH = "assets/image3_heatmap.png"
+IMAGE_RAW_PATH = "assets/image1.png"
+IMAGE_HEATMAP_PATH = "assets/image1_heatmap.jpeg"
 MODEL_ID = "llava-hf/llava-1.5-7b-hf"
